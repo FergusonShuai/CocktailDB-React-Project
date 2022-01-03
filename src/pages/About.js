31d542ca8bbf4cd1b66bc9d984b153792bdf89e2
Ferguson Shuai <ferguson_shuai@hotmail.com>
2022-01-03 10:30:37 -0800

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veniam hic
+        accusamus reiciendis debitis eveniet perspiciatis, deserunt minima
+        obcaecati aperiam dolorem atque aliquid nesciunt nihil illo cum ipsa
+        tempora autem.
+      </p>
+    </section>
+  );
+};
+
+export default About;
